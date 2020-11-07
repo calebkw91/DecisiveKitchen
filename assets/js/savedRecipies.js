18b01dc0b07c4c40f273bfb38242125324caef0b
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-11-07 12:15:56 -0600

    Initial commit, with comment and alert call to show js works

---
+++ assets/js/savedRecipies.js
@@ -0,0 +1,6 @@
+// for reference
+// will get saved recepies from local storage
+// then will call them to display in savedRecipies.html
+
+// Call alert to make sure it is connected
+alert("This work");
