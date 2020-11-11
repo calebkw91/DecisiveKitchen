03f73da7fc2902079d1a6b8982d4ec67d0728fbd
calebkw <walke1291@gmail.com>
2020-11-10 19:13:25 -0600

    Created function to load saved recipes from local storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -109,4 +109,12 @@ function saveRecipe(recipeID){
     }
 }
 
+function loadRecipes(){
+    let savedRecipes = [];
+    if (localStorage.getItem("savedRecipes") !== null){
+        savedRecipes = savedRecipes.concat(JSON.parse(localStorage.getItem("savedRecipes")));
+        //Load saved recipes into HTML elements
+    }
+}
+
 findRecipeID("52772");
