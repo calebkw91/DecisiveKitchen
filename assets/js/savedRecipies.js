768ef8bfaf86870ac95cfa5f6d48dfb9a3d15f2e
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-11-10 20:10:45 -0600

    get the hide and show working

--- assets/js/savedRecipies.js
+++ assets/js/savedRecipies.js
@@ -5,7 +5,35 @@
 // Call alert to make sure it is connected
 //console.log("This work");
 
-$(".recipieName").on("click", function() {
-    $("#recipie1").removeClass("hide");
-    //alert("This work!")
-});
+function showRecipie () {
+    $(".recipieName").on("click", function() {
+        let state = $(this).attr("data-state");
+
+        if (state === "hide") {
+            //$(".recipie").removeClass("hide");
+            $("#recipie" + $(this).attr("target")).hide();
+            $(this).attr("data-state", "show");
+            
+        } else {
+            //$(".recipie").addClass("hide");
+            //$(".recipie").show();
+            $("#recipie" + $(this).attr("target")).show();
+            $(this).attr("data-state", "hide");
+        }
+
+        // $("#recipie1").removeClass("hide");
+
+        
+        // if (state === "still") {
+        //     $(this).attr("src", $(this).attr("data-animate"));
+        //     $(this).attr("data-state", "animate");
+        //   } else {
+        //     $(this).attr("src", $(this).attr("data-still"));
+        //     $(this).attr("data-state", "still");
+        //   }
+
+
+    });
+};
+
+showRecipie();
