50d51eebf7104eb5bba6bb081449ebf857ddc92e
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-11-10 20:15:57 -0600

    Remove commented code as it is not longer needed, and added new comment to descript what is happening

--- assets/js/savedRecipies.js
+++ assets/js/savedRecipies.js
@@ -2,38 +2,22 @@
 // will get saved recepies from local storage
 // then will call them to display in savedRecipies.html
 
-// Call alert to make sure it is connected
-//console.log("This work");
 
+// Function to show or hide recipie
 function showRecipie () {
     $(".recipieName").on("click", function() {
         let state = $(this).attr("data-state");
 
         if (state === "hide") {
-            //$(".recipie").removeClass("hide");
             $("#recipie" + $(this).attr("target")).hide();
             $(this).attr("data-state", "show");
-            
         } else {
-            //$(".recipie").addClass("hide");
-            //$(".recipie").show();
             $("#recipie" + $(this).attr("target")).show();
             $(this).attr("data-state", "hide");
         }
 
-        // $("#recipie1").removeClass("hide");
-
-        
-        // if (state === "still") {
-        //     $(this).attr("src", $(this).attr("data-animate"));
-        //     $(this).attr("data-state", "animate");
-        //   } else {
-        //     $(this).attr("src", $(this).attr("data-still"));
-        //     $(this).attr("data-state", "still");
-        //   }
-
-
     });
 };
 
+// call function
 showRecipie();
