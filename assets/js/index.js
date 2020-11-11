53bca8c6774c844037353ba38f7f4ac9999d3523
calebkw <walke1291@gmail.com>
2020-11-10 19:21:07 -0600

    Created file for savedRecipes and moved function in there

--- assets/js/index.js
+++ assets/js/index.js
@@ -109,12 +109,4 @@ function saveRecipe(recipeID){
     }
 }
 
-function loadRecipes(){
-    let savedRecipes = [];
-    if (localStorage.getItem("savedRecipes") !== null){
-        savedRecipes = savedRecipes.concat(JSON.parse(localStorage.getItem("savedRecipes")));
-        //Load saved recipes into HTML elements
-    }
-}
-
 findRecipeID("52772");
