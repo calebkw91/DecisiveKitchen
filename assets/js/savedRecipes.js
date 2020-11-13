520f68c7c6d120e24563b94a088d7f0cd8038419
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-11-12 19:53:14 -0600

    testing to load recipes within array

--- assets/js/savedRecipes.js
+++ assets/js/savedRecipes.js
@@ -25,9 +25,53 @@ showRecipe();
 loadRecipes();
 
 function loadRecipes(){
-    let savedRecipes = [];
-    if (localStorage.getItem("savedRecipes") !== null){
-        savedRecipes = savedRecipes.concat(JSON.parse(localStorage.getItem("savedRecipes")));
-        //Load saved recipes into HTML elements
+    // let savedRecipes = [];
+    // if (localStorage.getItem("savedRecipes") !== null){
+    //     savedRecipes = savedRecipes.concat(JSON.parse(localStorage.getItem("savedRecipes")));
+    //     //Load saved recipes into HTML elements
+    // }
+}
+
+
+
+function findRecipeID(loadRecipes){
+    let arrayRecipes = [52772, 52771, 53016]
+    for (let i = 0; i < arrayRecipes.length; i++){
+    let queryURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + arrayRecipes[i];
+
+
+    $.ajax({
+        url: queryURL, 
+        method: "GET"
+    })
+    .then(function(response) {
+
+        console.log(response);
+        console.log(response.meals[0].strMeal)
+
+
+
+        
+
+
+        // // Print recipe name
+        // $("#recipeName1").text(response.meals[0].strMeal);
+
+        // // Print image recipieImage  response.meals[0].strMealThumb
+
+        // //let image = $("<img>").attr("src", response.meals[0].strMealThumb);
+
+        // $(".recipieImage").attr("src", response.meals[0].strMealThumb);
+
+
+        // // Print instructions
+        // $(".writtenDirection").text(response.meals[0].strInstructions);   
+
+        // // print YT link into videoLink class
+        // $(".videoLink").attr("href", response.meals[0].strYoutube);
+        
+    });
     }
 }
+
+findRecipeID();
